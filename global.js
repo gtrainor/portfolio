ace7358cf8dc481c34952798b9f028bee37a324a
gtrainor <141669790+gtrainor@users.noreply.github.com>
2025-04-24 09:16:19 -0700

    almost done w lab 4

--- global.js
+++ global.js
@@ -79,6 +79,61 @@ document.body.insertAdjacentHTML(
     select.value = localStorage.colorScheme;
   }
 
-  
+  const usernameInput = document.querySelector('#temp');
 
-  
+
+  usernameInput?.addEventListener('submit', function (event) {
+    event.preventDefault(); 
+    let data = new FormData(usernameInput);
+
+    let params = [];
+    
+    for (let [name, value] of data) {
+      const f_name = encodeURIComponent(name);
+      const email = encodeURIComponent(value);
+      // const url = `mailto:test@example.com?subject=Contact%20Form&body=Name:${f_name}%0AEmail:${email}`;
+      params.push(`${f_name}=${email}`);
+      console.log(name, email);
+      
+    };
+
+    let url = usernameInput.action + '?' + params.join('&');
+    window.location.href = url;
+  });
+
+  export async function fetchJSON(url) {
+    try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      console.log(response);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      };
+      const data = await response.json();
+      return data;
+      
+    } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+    }
+  }
+
+  export function renderProjects(project, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+
+  for (let p of project){
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+      <${headingLevel}>${p.title || 'Untitled Project'}</${headingLevel}>
+      <img src="${p.image || 'placeholder.jpg'}" alt="${p.title || 'No title'}">
+      <p>${p.description || 'No description available.'}</p>
+    `;
+
+    containerElement.appendChild(article);
+    
+  };
+  }
+
+  export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);// return statement here
+  }
